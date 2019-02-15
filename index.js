0ed683b86933ee41ccd4e0e470123a4b9081e0fb
Carla Stickler <carlastickler@gmail.com>
2019-02-15 01:09:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const logWhisper = (string) => {
   console.log(string.toLowerCase())
 }
 const sayHiToGrandma = (string) => {
-  if (string.toLowerCase()) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!";
-  } else if (string.toUpperCase()) {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
